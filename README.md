72c71320e6a4200e7f86fda55eeccc0814b2147f
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-01-19 14:27:16 +0800

    created grid

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ETCH-A-SKETCH
+//printing gridk
+-webpage with 16x16 grid of square divs using JS
+
+-put them in a container
+
+-use flexbox to make it appera like a grid
+
+-use the add className technique
+
+
+//getting the size of the grid
+
+-create a button that'll create a pop up asking how many grid on each side they want.
+
+-must be less than 100
+
+-should be able to create 64 x 64 grid when asked
--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 const container = document.querySelector('.container');
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        let box = document.createElement('div');
-        box.classList.add('grids');
-        container.appendChild(box);    }
+const section = document.querySelector('.section');
+const button = document.querySelector('.button');
 
-    let box = document.createElement('div');
-    box.classList.add('grids');
-    container.appendChild(box);
-    
-}
-
-// window.addEventListener('mouseover',(e) =>{
-//     e.target.style.border = "red 2px solid";
-//     console.log(e.target)
+let size = 64;
+// button.addEventListener('click',() => {
+//      size =  parseInt(prompt("Enter size: "))
+//      console.log(typeof size);
 // })
 
 
-container.addEventListener('mouseover',(e)=>{
-    if(!e.target.classList.contains('grids')) return;
-    e.target.style.backgroundColor = 'red';
+console.log(container.clientWidth)
+console.log(container.clientHeight)
+
+
+
+
+createGrid();
+function createGrid(){
+    
+    //for row
+    for(let i = 0; i < size; i++){
+    //for column
+        for(let j = 1; j < size; j++){
+            let box = document.createElement('div');
+            box.classList.add("grid");
+            container.appendChild(box);
+            box.style.width = `${960 / size}px`;
+            box.style.height = `${960 / size}px`;
+        }
+    let box = document.createElement('div');
+    box.classList.add("grid");
+    container.appendChild(box);
+    box.style.width = `${960 / size}px`;
+    box.style.height = `${960 / size}px`;
+}};
+
+container.addEventListener('mouseover',(e) => {
+    if(!e.target.classList.contains('grid')) return;
+    console.log(e.target)
+    e.target.classList.add('hover')
 })
